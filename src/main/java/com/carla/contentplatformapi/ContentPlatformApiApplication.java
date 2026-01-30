cfb78083681623d124c7028609cb9941269fa2cd
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-30 14:11:26 -0300

    Initial project setup

---
+++ src/main/java/com/carla/contentplatformapi/ContentPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.carla.contentplatformapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentPlatformApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentPlatformApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carla/contentplatformapi/ContentPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carla.contentplatformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentPlatformApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

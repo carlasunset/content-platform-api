2d11b068332f495c24667ff5bf158ecc54f98fa1
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-01-30 15:34:54 -0300

    feat: seed users data on application startup

---
+++ src/main/java/com/carla/contentplatformapi/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.carla.contentplatformapi.config;
+
+import com.carla.contentplatformapi.domain.User;
+import com.carla.contentplatformapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        repository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        repository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}

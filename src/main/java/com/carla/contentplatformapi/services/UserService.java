be18a895f27f314d78314cd01f9eabf8e5fed1d5
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-02-03 13:41:32 -0300

    feat: implement findById endpoint and standardized error response for non-existent resources

--- src/main/java/com/carla/contentplatformapi/services/UserService.java
+++ src/main/java/com/carla/contentplatformapi/services/UserService.java
@@ -2,10 +2,12 @@ package com.carla.contentplatformapi.services;
 
 import com.carla.contentplatformapi.domain.User;
 import com.carla.contentplatformapi.repository.UserRepository;
+import com.carla.contentplatformapi.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,11 @@ public class UserService {
 
     public List<User> findAll(){
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
     }
 
 }

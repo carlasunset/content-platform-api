eedcf005bfd712dc7a6ccb2c76b09bdcce6433ab
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-02-09 19:54:34 -0300

    feat: add endpoint to fetch posts by user id

--- src/main/java/com/carla/contentplatformapi/resources/UserResource.java
+++ src/main/java/com/carla/contentplatformapi/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.carla.contentplatformapi.resources;
 
+import com.carla.contentplatformapi.domain.Post;
 import com.carla.contentplatformapi.domain.User;
 import com.carla.contentplatformapi.dto.UserDTO;
 import com.carla.contentplatformapi.services.UserService;
@@ -51,5 +52,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }

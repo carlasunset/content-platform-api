47442ccc09b46ed3f45cc75188871c77b7624216
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-02-09 19:31:53 -0300

    refactor(post): decouple Post from User by introducing AuthorDTO

--- src/main/java/com/carla/contentplatformapi/config/Instantiation.java
+++ src/main/java/com/carla/contentplatformapi/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.carla.contentplatformapi.config;
 
 import com.carla.contentplatformapi.domain.Post;
 import com.carla.contentplatformapi.domain.User;
+import com.carla.contentplatformapi.dto.AuthorDTO;
 import com.carla.contentplatformapi.repository.PostRepository;
 import com.carla.contentplatformapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,11 +33,10 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-        Post post1 = new Post(null, format.parse("21/03/2018"), "Trip time!", "I'm traveling to São Paulo. See you!", maria);
-        Post post2 = new Post(null, format.parse("23/03/2018"), "Good morning!", "Feeling happy this morning!", maria);
-
         repository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, format.parse("21/03/2018"), "Trip time!", "I'm traveling to São Paulo. See you!", new AuthorDTO(maria));
+        Post post2 = new Post(null, format.parse("23/03/2018"), "Good morning!", "Feeling happy this morning!", new AuthorDTO(maria));
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }

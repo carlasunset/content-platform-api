777788699625b4aac355238d0fdfd9f4607fe584
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-02-10 12:58:34 -0300

    feat: implement title search endpoint using query methods

--- src/main/java/com/carla/contentplatformapi/resources/PostResource.java
+++ src/main/java/com/carla/contentplatformapi/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.carla.contentplatformapi.resources;
 
 import com.carla.contentplatformapi.domain.Post;
+import com.carla.contentplatformapi.resources.util.URL;
 import com.carla.contentplatformapi.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,6 +20,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }

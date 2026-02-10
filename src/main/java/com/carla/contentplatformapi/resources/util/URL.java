9c5ce4f3ce48419ecfb3b93be163624b04af4a20
Carla Lira <109699098+carlasunset@users.noreply.github.com>
2026-02-10 13:42:30 -0300

    feat: implement advanced search with multiple criteria using MongoDB @Query

--- src/main/java/com/carla/contentplatformapi/resources/util/URL.java
+++ src/main/java/com/carla/contentplatformapi/resources/util/URL.java
@@ -3,11 +3,25 @@ package com.carla.contentplatformapi.resources.util;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class URL {
 
     public static String decodeParam(String text){
         return URLDecoder.decode(text, StandardCharsets.UTF_8);
+    }
+
+    public static Date convertDate(String textDate, Date defaultValue){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        format.setTimeZone(TimeZone.getTimeZone("GMT"));
+        try {
+            return format.parse(textDate);
+        } catch (ParseException e) {
+            return defaultValue;
+        }
     }
 
 }
